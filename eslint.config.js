2a68664e1f7ad7dc9a6c454ff78eaffa0dd4625a
mk.kim <mk.kim@vogoplay.com>
2025-08-20 17:46:54 +0900

    feat: Configure ESLint and Prettier for code quality and consistency

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint'; // Use the new recommended way to import
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// mimic CommonJS variables -- not available in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    resolvePluginsRelativeTo: __dirname,
+});
+
+export default tseslint.config(
+    {
+        files: ['**/*.ts'],
+        extends: [
+            ...tseslint.configs.recommended, // Use the recommended configs from typescript-eslint
+            eslintPluginPrettierRecommended,
+        ],
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                project: ['./tsconfig.json'],
+            },
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+    },
+    {
+        // Ignore build output and node_modules
+        ignores: ['dist/**', 'node_modules/**'],
+    },
+);
