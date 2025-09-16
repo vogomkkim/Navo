7f8790d8b2268bf5d966f55cf55fb4c57cbf611f
mk.kim <mk.kim@vogoplay.com>
2025-09-16 18:24:31 +0900

    config: dbCredentials 섹션 제거 및 코드 정리
    - `drizzle.config.ts` 파일에서 dbCredentials 섹션을 제거하여 코드의 간결성을 높였습니다. SSL 설정은 유지하였으며, 보안성을 고려한 변경입니다.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,10 +4,7 @@ export default defineConfig({
   schema: "./server/drizzle/schema.ts",
   out: "./server/drizzle",
   dialect: "postgresql",
-  dbCredentials: {
-    // url: "postgres://@pg-vico-vogoplay-681f-vico.k.aivencloud.com:24567/defaultdb?sslmode=require",
-    ssl: { rejectUnauthorized: false },
-  },
+  
   verbose: true,
   strict: true,
 });
